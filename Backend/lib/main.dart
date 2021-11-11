398f18e87ce40e2852b7b07b1810378a18507844
ErkinAbuzarli <56367952+ErkinAbuzarli@users.noreply.github.com>
2021-11-11 19:15:24 +0300

    User perspektivinden görünen ana ekranın templati yüklendi

---
+++ Backend/lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.cyan,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Konum',
+      style: optionStyle,
+    ),
+    Text(
+      'Mağaza',
+      style: optionStyle,
+    ),
+    Text(
+      'Sepet',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        extendBody: true,
+        appBar: AppBar(
+          title: const Text('Akşam Pazarı'),
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: Container(
+            margin: const EdgeInsets.all(5.0),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(40.0)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black38, spreadRadius: 0, blurRadius: 3),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(
+                 Radius.circular(50.0)
+              ),
+              child: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.map),
+                    label: 'Konum',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.store),
+                    label: 'Mağaza',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.shopping_basket),
+                    label: 'Sepet',
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.amber[78],
+                onTap: _onItemTapped,
+              ),
+            )));
+  }
+}
---
+++ Backend/lib/shop_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
