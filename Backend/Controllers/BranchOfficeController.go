54f4fae1c0cd39c1d202c80e6568a5981dbf7c37
sema29 <semanurboz5@gmail.com>
2021-12-12 18:27:01 +0300

    BranchOfficeController class olarak düzenlendi

--- Backend/Controllers/BranchOfficeController.go
+++ Backend/Controllers/BranchOfficeController.go
@@ -2,12 +2,24 @@ package controllers
 
 import (
 	"net/http"
+	database "varsa/Database"
 )
 
-func AddProductToStorage(w http.ResponseWriter, r *http.Request){
+type BranchOfficeController struct {
+	DB database.Posgres
+}
+
+func NewBranchOfficeController(_DB database.Posgres) BranchOfficeController{
+	b :=BranchOfficeController {}
+	b.DB = _DB
+	return b
+}
+
+
+func (b*BranchOfficeController) AddProductToStorage(w http.ResponseWriter, r *http.Request){
 
 }
 
-func GetBranchProducts(w http.ResponseWriter, r *http.Request){
+func (b*BranchOfficeController) GetBranchProducts(w http.ResponseWriter, r *http.Request){
 
 }
