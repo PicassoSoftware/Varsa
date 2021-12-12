7e1cd647bb103d6c8bc00499f868a7b35c340880
haticeko <kovanhatice880@gmail.com>
2021-12-12 18:29:29 +0300

    CustomerController class olarak düzenlendi.

--- Backend/Controllers/CustomerController.go
+++ Backend/Controllers/CustomerController.go
@@ -2,23 +2,34 @@ package controllers
 
 import (
 	"net/http"
+	database "varsa/Database"
 )
 
-func NearbyProducts(w http.ResponseWriter, r *http.Request) {
+type CustomerController struct {
+    DB database.Posgres
+}
+
+func NewCustomerController(_DB database.Posgres) CustomerController{
+	c := CustomerController{}
+	c.DB = _DB
+	return c
+}
+
+func (c*CustomerController) NearbyProducts(w http.ResponseWriter, r *http.Request) {
 
 }
-func GetProduct(w http.ResponseWriter, r *http.Request) {
+func (c*CustomerController) GetProduct(w http.ResponseWriter, r *http.Request) {
 
 }
-func AddProductToCart(w http.ResponseWriter, r *http.Request) {
+func (c*CustomerController) AddProductToCart(w http.ResponseWriter, r *http.Request) {
 
 }
-func ReserveProduct(w http.ResponseWriter, r *http.Request) {
+func (c*CustomerController) ReserveProduct(w http.ResponseWriter, r *http.Request) {
 	println(r.Body)
 }
-func GetCart(w http.ResponseWriter, r *http.Request) {
+func (c*CustomerController) GetCart(w http.ResponseWriter, r *http.Request) {
 
 }
-func DeleteProductFromCart(w http.ResponseWriter, r *http.Request) {
+func (c*CustomerController) DeleteProductFromCart(w http.ResponseWriter, r *http.Request) {
 
 }
