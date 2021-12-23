7d6abe11686ef336fbce721dfc9aa757b652b7b4
ErkinAbuzarli <56367952+ErkinAbuzarli@users.noreply.github.com>
2021-12-23 15:57:00 +0300

    onTap eventi card a eklendi

--- Frontend/FrontendCostumer/lib/widgets/product_for_store.dart
+++ Frontend/FrontendCostumer/lib/widgets/product_for_store.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:ui/assets/const.dart';
+import 'package:ui/views/product_details.dart';
 
 class ProductForStore extends StatelessWidget {
   final int index = 0;
@@ -10,76 +11,82 @@ class ProductForStore extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 5,
-        color: kBackgroundColor,
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Image.asset("lib/assets/milk.jpg"),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                //crossAxisAlignment: CrossAxisAlignment.baseline,
-                //textBaseline: TextBaseline.alphabetic,
-                children: [
-                  Text(
-                    "Süt ",
-                    style: TextStyle(
-                        color: kPrimaryColor,
-                        fontSize: 20,
-                        fontFamily: "Lato"),
-                  ),
-                ],
-              ),
-              Row(
-                //crossAxisAlignment: CrossAxisAlignment.baseline,
-                //textBaseline: TextBaseline.alphabetic,
-                children: [
-                  Text(
-                    "Stock: 15",
-                    style: TextStyle(
-                      color: kSecondaryColor,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.baseline,
-                textBaseline: TextBaseline.alphabetic,
-                children: [
-                  RichText(
-                    text: TextSpan(
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ProductDetails()),
+      ),
+      child: Card(
+          elevation: 5,
+          color: kBackgroundColor,
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Image.asset("lib/assets/milk.jpg"),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  //crossAxisAlignment: CrossAxisAlignment.baseline,
+                  //textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text(
+                      "Süt ",
                       style: TextStyle(
-                        fontSize: 14.0,
-                        color: kPrimaryColor,
+                          color: kPrimaryColor,
+                          fontSize: 20,
+                          fontFamily: "Lato"),
+                    ),
+                  ],
+                ),
+                Row(
+                  //crossAxisAlignment: CrossAxisAlignment.baseline,
+                  //textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text(
+                      "Stock: 15",
+                      style: TextStyle(
+                        color: kSecondaryColor,
+                        fontSize: 14,
                       ),
-                      children: const <TextSpan>[
-                        TextSpan(text: 'Son '),
-                        TextSpan(
-                          text: '5 ',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.red),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: kPrimaryColor,
                         ),
-                        TextSpan(text: 'gün'),
-                      ],
+                        children: const <TextSpan>[
+                          TextSpan(text: 'Son '),
+                          TextSpan(
+                            text: '5 ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, color: Colors.red),
+                          ),
+                          TextSpan(text: 'gün'),
+                        ],
+                      ),
                     ),
-                  ),
-                  Text(
-                    "10₺",
-                    style: TextStyle(
-                      color: kPrimaryColor,
-                      fontSize: 26,
+                    Text(
+                      "10₺",
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontSize: 26,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+                  ],
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
